70f797bc9721c1369255e6e9340196847dc1a424
GoRhanHee <gorhanhee01@gmail.com>
2026-02-10 18:37:30 +0900

    build: import build system

--- build.sh
+++ build.sh
@@ -13,12 +13,39 @@ git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_a
 git clone https://github.com/dandelion64-Archives/clang-r353983c \
  toolchain/clang/host/linux-x86/clang-r353983c
 
+# Setting 
+export ANDROID_BUILD_TOP=$(pwd)
+export OPTION=$1
+
 # OEM Setting
 export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
 export ARCH=arm64
 
-make exynos980-a51x_defconfig
-make
+CROSS_COMPILE=${ANDROID_BUILD_TOP}/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+CLNAG=${ANDROID_BUILD_TOP}/toolchain/clang/host/linux-x86/clang-r353983c/bin/clang
+CLANG_TRIPLE=aarch64-linux-gnu-
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+# Cooking Kernel Source
+DEFCONFIG="exynos980-a51x_defconfig"
+
+if [ "${OPTION}" == "ksu" ]; then
+    CONFIGS="${DEFCONFIG} gorhanhee.config ksu.config"
+elif [ "${OPTION}" == "apatch" ]; then
+    CONFIGS="${DEFCONFIG} gorhanhee.config"
+else
+    echo "Write Build Option ex) ./build.sh ksu"
+    exit 1    
+fi
+
+MAKE_ARGS="
+-j16 \
+ARCH=arm64 \
+CROSS_COMPILE=$CROSS_COMPILE \
+CC=$CLNAG \
+CLANG_TRIPLE=$CLANG_TRIPLE \
+O=out
+"
+
+make ${MAKE_ARGS} ${CONFIGS} || exit 1
+make ${MAKE_ARGS} || exit 1
